5fab7f2629ace8445298c397ff1589d3b8316510
akr <akr@toshayu.srisrisri.in>
2019-07-05 14:05:42 +0530

    taab

--- src/main/java/in/srisrisri/clinic/medicineStock/MedicineStockEntity.java
+++ src/main/java/in/srisrisri/clinic/medicineStock/MedicineStockEntity.java
@@ -7,6 +7,7 @@ import java.math.BigDecimal;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
@@ -14,10 +15,11 @@ import javax.persistence.Transient;
 
 @Entity(name = "MedicineStock")
 @Table(name = "MedicineStock")
+
 public class MedicineStockEntity implements Serializable {
 
     @Id
-    @GeneratedValue
+   @GeneratedValue(strategy=GenerationType.TABLE, generator="tab1")
     private long id;
 
     @OneToOne
